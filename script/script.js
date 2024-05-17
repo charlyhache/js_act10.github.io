052f05db1ff78a58fb4b5604b83df5b61fe3852b
charlyhache <167575346+charlyhache@users.noreply.github.com>
2024-05-17 09:44:51 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,66 @@
+
+//var numero=4; //number
+
+//var boolean="true"; //boolean
+//var nothing=null
+//var paises=["argentina", "brasil", "uruguay"];
+//var ciudades=["buenos aires","san pablo","montevideo","lima"]
+
+//var numero_1=9;
+//var numero_2=4;
+//var suma=numero_1+numero_2;
+//alert("el resultado de la suma es" + suma)
+//alert("bienvenido a " + paises [0]   +  ", visiten " + ciudades[0])
+
+
+//function suma(numero1,numero2,numero3){
+    //var suma=numero1+numero2+numero3
+    //alert("la suma es " + suma)
+//}
+
+//suma(3,3,3)}
+
+
+//var vaso="lleno"
+//function beber(){
+//    console.log("el vaso esta " + vaso)
+//}
+
+//beber()
+
+//var usuario={
+//    username:"Carlos",
+//    puntos":30,
+//    partidosjugados:2,
+//    profecional:"false"
+
+
+//}
+
+//alert("el nombre del uuario es " +usuario.username)
+//alert("los puntos del partido fueron  " +usuario.puntos)
+
+//const PI=3.1415
+//PI=3
+//console.log("valor de PI: " + PI)
+
+//var edad=Number(prompt("por favor ingese edad"))
+//if(edad>=18){
+//    alert("puedes ingresar a nuestra pagina web")
+//} else{
+//    alert("no puedes ingresar a nuestra pagina web")
+//}
+
+var nombre = "charly"; //string
+var apellido = "hache"; //string
+console.log(nombre+ " " + apellido);
+
+var numero_1=9;
+var numero_2=4;
+
+var suma=numero_1+numero_2;
+console.log(suma);
+
+var numero_3=87 + suma;
+console.log(numero_3);
+
